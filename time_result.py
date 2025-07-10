fc2e99236169421ef5d1fe2d6364d121e7a2b6ca
PierreFumeron <pierre.fumeron@club-internet.fr>
2025-07-10 15:24:45 +0200

    modified barplot to make it dynamic

--- time_result.py
+++ time_result.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 import argparse
 from pathlib import Path
+import os
+import numpy as np
 
 
 def compare_time_per_label(file):
@@ -21,40 +22,45 @@ def compare_time_per_label(file):
 
 
 def compare_time_per_method():
-    # TODO get all *_runtime.csv files from result folders instead of hardcoding paths
-    datasets = ["tcga", "meth", "pbmc"]
+
+    file_names = [
+        fn for fn in os.listdir("result") if fn.split("_")[-1] == "runtime.csv"
+    ]
     dfs = []
-    for dataset in datasets:
-        df_tmp = pd.read_csv(f"result/{dataset}_runtime.csv", index_col=0)
+    for dataset in file_names:
+        df_tmp = pd.read_csv(f"result/{dataset}", index_col=0)
         df_tmp["mean"] = df_tmp.mean(axis=1)
         df_tmp["std"] = df_tmp.std(axis=1)
-        df_tmp["dataset"] = dataset
+        df_tmp["dataset"] = dataset.split("_")[0]
 
         dfs.append(df_tmp[["mean", "std", "dataset"]])
     df_runtime = pd.concat(dfs)
     df_runtime["method"] = df_runtime.index
-
-    sns.barplot(data=df_runtime, x="dataset", y="mean", hue="method", errorbar=None)
-
-    a = 0
-    for i, row in df_runtime.iterrows():
-        # TODO change formula for errorbars to make it flexible
+    df_runtime = df_runtime[df_runtime["method"].isin(["cosg"])]
+    datasets = df_runtime["dataset"].unique()
+    methods = df_runtime["method"].unique()
+    n_methods = len(methods)
+    bar_width = 0.8 / n_methods
+    for i, method in enumerate(methods):
+        x = np.arange(len(datasets)) + (i - n_methods / 2) * bar_width + bar_width / 2
+        method_df = df_runtime[df_runtime["method"] == method]
+        plt.bar(x, method_df["mean"], width=bar_width, label=method)
         plt.errorbar(
-            x=a // 4 + (-0.3 + 0.2 * (a % 4)),
-            y=row["mean"],
-            yerr=row["std"],
+            x,
+            method_df["mean"],
+            yerr=method_df["std"],
             fmt="none",
             capsize=5,
             color="black",
         )
-        a += 1
+
+    plt.xticks(ticks=np.arange(len(datasets)), labels=datasets)
     plt.xlabel("Dataset")
     plt.ylabel("Average runtime (s)")
     plt.title("Feature selection methods' average runtime across different datasets")
     plt.legend(title="Method")
     plt.grid(axis="y", linestyle="--", alpha=0.7)
-
-    plt.savefig("foo.png")
+    plt.savefig("plots/time_comparison.png")
     plt.show()
 
 
